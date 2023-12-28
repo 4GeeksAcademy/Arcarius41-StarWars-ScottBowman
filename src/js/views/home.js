73b8e177f3dff7d644de2b19eb53d28827705d46
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2023-12-28 12:09:07 +0000

    getting set up

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,7 +3,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
+	<div data-bs-theme="dark"  className= "text-center mt-5">
 		<h1>Hello Rigo!</h1>
 		<p>
 			<img src={rigoImage} />
@@ -13,3 +13,4 @@ export const Home = () => (
 		</a>
 	</div>
 );
+
