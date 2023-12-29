5a8ca2b2baa5ebb8d77d4dfffd8e9112e7c05a72
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2023-12-29 22:44:28 +0000

    tried adding a fontawesome icon

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-dark bg-dark mb-3">
-			<Link to="/">
+			<i className="fa-solid fa-jedi" style={{ color: "#51291f" }}></i>
+			{/* <Link to="/">
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
+			</Link> */}
+			
 			<div className="ml-auto">
 				<Link to="/demo">
 					<button className="btn btn-primary">Check the Context in action</button>
